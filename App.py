ba5143e317cec1be12378ad65bf4d7c90ae496a1
Adithya-2024 <sadithya.s256@gmail.com>
2024-03-22 20:09:28 +0530

    Changed the code

--- App.py
+++ App.py
@@ -1,4 +1,8 @@
-print("Kalam")
-print("Hi")
-if 0<1:
-    print("point")
+print("Hello !welcome to python world ")
+print("Tho check the given number is odd or even ")
+num= int(input("Enter a number :"))
+if num%2==0:
+    print("The number is Even ")
+else:
+    print("The number is ODD")
+    
